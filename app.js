463c9727d0d8bd4b9a753ac3344a200126f6b1a2
bdkOussama <bdk.oussama@gmail.Com>
2017-12-03 23:40:21 +0100

    routes Error Handling

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.use(logger("dev"));
 app.use(jsonParser());
 app.use("/questions" , routes);
 
+// catch a 404 and forward to error handler
+app.use(function(req,res,next){
+    var err = new Error('Not Found ! ');
+    err.status = 404 ;
+    next(err);
+});
+
+//Error Handler
+app.use(function(err , req , res , next){
+    res.status(err.status || 500);
+    res.json({
+      error: {
+        message: err.message
+      }
+    });
+});
 
 app.listen(3000 , function(){
   console.log('express Server is listening on Port 3000');
--- routes.js
+++ routes.js
@@ -5,51 +5,49 @@ const router = express.Router();
 //GET questions
 //Route for questions collection
 router.get("/" ,( req , res ) =>{
-    res.json(
-      {
+    res.json({
         response : " you sent me a GET request "
       });
 });
+
 
 //POST questions
 //Route for Creating  questions
 router.post("/" ,( req , res ) =>{
     //Return all the questions
-    res.json(
-      {
+    res.json({
          response : " you sent me a POST request " ,
          body : req.body
     });
 });
 
+
 //GET questions/:id
 //Route for specific question
 router.get("/:qID" ,( req , res ) =>{
-    res.json(
-      {
+    res.json({
         response : " you sent me a GET request  for a specific ID"
       });
 });
+
 
 //POST /questions/:qID/answers
 //Route for Creating  answers
 router.post("/:qID/answers" ,( req , res ) =>{
     //Return all the questions
-    res.json(
-      {
+    res.json({
          response : " you sent me a POST request to /answers" ,
          questionId : req.params.qID,
         body : req.body
     });
 });
 
+
 //PUT /questions/:id/answers/:aID
 // Edit a specific answer
-
 router.put("/:qID/answers/:aID" , ( req , res ) =>{
     //Return all the questions
-    res.json(
-      {
+    res.json({
          response : " you sent me a PUT request to /answers" ,
          questionId : req.params.qID,
          answerID : req.params.aID,
@@ -57,26 +55,32 @@ router.put("/:qID/answers/:aID" , ( req , res ) =>{
     });
 });
 
+
 //DELETE /questions/:id/answers/:aID
 // delete a specific answer
-
 router.delete("/:qID/answers/:aID" , ( req , res ) =>{
     //Return all the questions
-    res.json(
-      {
+    res.json({
          response : " you sent me a DELETE request to /answers" ,
          questionId : req.params.qID,
          answerID : req.params.aID
     });
 });
 
+
 //POST /questions/:id/answers/:aID/vote-up
 //POST /questions/:id/answers/:aID/vote-down
 // vote on a  specific answer
-router.post("/:qID/answers/:aID/vote-:dir" , ( req , res ) =>{
-    //Return all the questions
-    res.json(
-      {
+router.post("/:qID/answers/:aID/vote-:dir" , ( req , res , next ) =>{
+      if(req.params.dir.search(/^(up|down)$/) === -1 ){
+            var err = new Error('Oups Wrong vote Url');
+            err.status = 404 ;
+             next(err);
+      }else{
+            next();
+      }
+} ,( req , res ) =>{
+    res.json({
          response : " you sent me a POST request to /vote" + req.params.dir ,
          questionId : req.params.qID,
          answerID  : req.params.aID,
